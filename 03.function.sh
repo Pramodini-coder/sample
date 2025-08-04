f18d2b39a919a0ab034299c4c4b31c8879c902c4
pramodini <pramodinithurpu95@gmail.com>
2025-08-04 07:47:17 -0500

    roboshop code

--- 03.function.sh
+++ 03.function.sh
@@ -6,3 +6,19 @@ SAMPLE
 
 source common.sh
 demo
+
+# variable decleare out side of funtion can be accleable inside the funtion vice -aversa
+# before excution
+# LHS  funtion  RHS
+abc1() {
+echo value of a -$a
+echo value of a -$b
+echo first argument - $1
+echo secode argument -$2
+echo all argument - $*
+echo count argument $#
+}
+a=10
+b=20
+abc1 100 200
+
