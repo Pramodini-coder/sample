cf387abade959f41dfd497c69d8a34ead11ad2da
pramodini <pramodinithurpu95@gmail.com>
2025-08-04 07:23:51 -0500

    roboshop code

--- 02.variable.sh
+++ 02.variable.sh
@@ -1,2 +1,17 @@
 x=100
+#access the avaraiab;e -$var 0r ${var}
+
 echo value x - $x
+echo value x - ${x}
+
+#input for user
+#before script
+#LHS        script    RHS
+# before script  - environment variable
+#on cli I sent the variable using the following the command
+#export - a_env=1000
+#bash 02.available.sh to run the command in linux
+# shellcheck disable=SC2086
+echo env variable - a_env  - ${a-env}
+
+
