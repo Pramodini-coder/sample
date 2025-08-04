959d1d8c256df071b1f735d2705345f515c646e5
pramodini <pramodinithurpu95@gmail.com>
2025-08-04 07:49:24 -0500

    roboshop code

--- 03.function.sh
+++ 03.function.sh
@@ -11,12 +11,12 @@ demo
 # before excution
 # LHS  funtion  RHS
 abc1() {
-echo value of a -$a
-echo value of a -$b
+echo value of a - $a
+echo value of a - $b
 echo first argument - $1
-echo secode argument -$2
+echo secode argument - $2
 echo all argument - $*
-echo count argument $#
+echo count argument - $#
 }
 a=10
 b=20
